13ceeae555b0c37cba3ba717d5e06ae4dfc5674b
Dman89 <upnvapesd@gmail.com>
2016-11-27 10:36:09 -0800

    Set Up Files For Time Stamp

---
+++ TimeStamp/app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var Application = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+})
+
+module.exports = Application
---
+++ TimeStamp/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [
+  {
+    context: __dirname,
+    entry: {app: ['./app/app.jsx']},
+    devServer: { inline: true },
+    output: {
+      path: path.resolve(__dirname, "public"),
+      filename: 'scripts/TimeStamp.bundle.js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx$/,
+          exclude: /(node_modules)/,
+          loader: ['babel-loader'],
+          query: {
+            presets: ['react', 'es2015']
+          }
+        },
+        {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('css!sass')
+        }
+      ]
+    },
+    plugins: [
+      new webpack.OldWatchingPlugin(),
+      new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"scripts/vendor.bundle.js"),
+      new ExtractTextPlugin('styles/css/master.css', { allChunks: true })
+    ]
+  }
+]
